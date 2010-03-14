c10149b1d0ee5aa5cb7f8f1ab10deb25263b1849
Rizwan Reza <rizwanreza@gmail.com>
2010-03-14 22:34:44 +0330

    capdeploy added

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,23 @@
-# FIXME: common deployment stuff here, with a conditional load of the real
-# recipe file, so that the real stuff can exist locally without being
-# checked into the main repo.
+set :application, "bucketwise.rizwanreza.com"
+set :db_name, "bucketwise"
+set :repository,  "git@github.com:rizwanreza/bucketwise.git"
+set :port, 1111
+set :use_sudo, false
 
-capfile = File.expand_path("~/.bucketwise/Capfile")
-load(capfile) if File.exists?(capfile)
+set :domain, 'rizwanreza.com'
+set :user, 'deploy'
+set :deploy_to, "/home/#{user}/public/#{application}"
+
+set :scm, :git
+
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+
+namespace :deploy do
+  task :start do; end
+  task :stop do; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
